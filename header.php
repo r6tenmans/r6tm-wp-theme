bc18800b9f63bcdcf949aba72cc10ff75ca410ad
DeeHechEss <danialhaddadi@gmail.com>
2021-02-01 19:00:46 +0000

    Update fontawesome

--- header.php
+++ header.php
@@ -18,6 +18,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 
